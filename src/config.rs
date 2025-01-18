e6c66b09415e3d4176f433b532efbced7784ed82
RDOToole89 <roibinotoole@gmail.com>
2025-01-18 17:24:55 +0100

    Updated .gitignore

--- src/config.rs
+++ src/config.rs
@@ -3,10 +3,14 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 pub struct Config {
     pub server_addr: String,
+    pub firebase_project_id: String, // Firebase project ID
+    pub firebase_api_key: String,    // Firebase API key
 }
 
 pub fn load_config() -> Config {
     Config {
         server_addr: "127.0.0.1:3000".to_string(),
+        firebase_project_id: "your-firebase-project-id".to_string(),
+        firebase_api_key: "your-firebase-api-key".to_string(),
     }
 }
